b68e9f1c0a3ecdae1a43ef64ea70b0c6ab280c52
Margherita <mangoguglia@gmail.com>
2020-06-23 15:28:17 +0200

    js-dadi

---
+++ dadi/script.js
@@ -0,0 +1,32 @@
+//js-dadi creare un mini gioco in cui si lanciano due dadi, uno per il computer e uno per il giocatore e si determina chi ha vinto.
+
+var TiraDadi = document.getElementById('btn');
+TiraDadi.addEventListener ('click', function () {
+  var dado1;
+  var dado2;
+  var n = 0;
+  var m = 0;
+
+for (var i = 0; i < 3; i++) {
+  dado1 = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+  dado2 = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+}
+
+  if (dado1 > dado2){
+    m++;
+  } else if (dado2 > dado1){
+    n++;
+  }
+
+
+if (m > n){
+  console.log("vince dado 1");
+} else if (n > m){
+  console.log ("vince dado 2");
+} else {
+  console.log ("parità");
+}
+
+console.log(m);
+console.log(n);
+})
